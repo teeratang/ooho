aa9727447c84859be18e3604132bce9584ebaa08
sittisuk chartrasee <beerxp1@gmail.com>
2018-02-24 14:18:03 +0700

    new commit

---
+++ js/helper.js
@@ -0,0 +1,7 @@
+const valueId = (id) => document.getElementById(id).value;
+
+const resetId = (id) => document.getElementById(id).value = '';
+
+const locationHref = (id) => window.location.href = id;
+
+
