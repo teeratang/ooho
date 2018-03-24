4d03b7c5ab6433c9f4a0bf90ba3f92f765346115
sittisuk chartrasee <beerxp1@gmail.com>
2018-03-24 15:59:21 +0700

    add database

--- js/helper.js
+++ js/helper.js
@@ -4,4 +4,100 @@ const resetId = (id) => document.getElementById(id).value = '';
 
 const locationHref = (id) => window.location.href = id;
 
+const Footer = () => {
+  return `
+    <section class="hero is-light" style="box-shadow: 0px 1px 2px #c5c5c5 inset; background-color: #fafafa">
+      <div class="hero-body">
+        <div class="container">
+          <div class="columns" style="color: #777777;">
+            <div class="column is-2 has-text-centered">
+              <img src="../../images/icon/logo-index.png" alt="Logo" style="
+                position: relative;
+                left: 0px;
+                top: 30px;
+                width: 110px;
+                margin-bottom: 40px;
+              ">
+            </div>
+            <div class="column is-6">
+              <p>
+                ความทรงจำดีๆ คุณเก็บไว้ที่ไหน ? เรื่องราวดีๆ อาจมีหลงลืมไปได้ หากไม่ได้จดบันทึก
+                ประสบการณ์ ที่น่าแบ่งปัน อาจไม่มีใครได้รับรู้ หากคุณไม่ได้เป็นคนเล่าเรื่องราว
+                รายละเอียดของภาพถ่าย ในความรู้สึกเก่าๆ อาจมีลบเลือนไปบ้าง หากคุณเผลอละเลยไป
+              </p>
+              <p class="hightlight">#มาเขียน_มาแชร์_มาเล่า_กับเรา #OHO_Diary</p>
+              <p>© 2017 OHO Diary All Rights Reserved</p>
+            </div>
+            <div class="column is-4">
+              <table style="border-left-style:solid; border-left-width:1px">
+                <tr><td>&nbsp;นักท่องเที่ยวกว่า <strong>5,210</strong> คน</td></tr>
+                <tr><td>&nbsp;นักอ่านกว่า <strong>8,524,742</strong> คน</td></tr>
+                <tr><td>&nbsp;เรื่องราวกว่า <strong>8,450</strong> เรื่อง</td></tr>
+                <tr><td>&nbsp;โรงแรมกว่า <strong>25,571</strong> แห่ง</td></tr>
+                <tr><td>&nbsp;ร้านอาหารกว่า <strong>18,773</strong> แห่ง</td></tr>
+                <tr><td>&nbsp;สถานที่ท่องเที่ยว <strong>1,085</strong> แห่ง</td></tr>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <footer class="footer">
+      <div class="container">
+        <div class="content has-text-centered">
+          <div class="columns">
+            <div class="column is-9">
+              <div id="navMenuTransparentExample" class="navbar-menu">
+                <div class="navbar-start" style="position: absolute;bottom: -20px;">
+                  <div class="navbar-item has-dropdown is-hoverable">
+                    <a class="navbar-link white colorWhite font_footer_prompt_15 padding__LeftRight" href="#!" style="line-height: 38px;"> <!-- is-active -->
+                      Home
+                    </a>
+                  </div>
+                  <div class="navbar-item has-dropdown is-hoverable">
+                    <a class="navbar-link set-lightGreen lightGreen colorWhite font_footer_prompt_15 padding__LeftRight" href="#!">
+                      OHO Calendar
+                    </a>
+                  </div>
+                  <div class="navbar-item has-dropdown is-hoverable">
+                    <a href="#!" class="navbar-link set-orange orange colorWhite font_footer_prompt_15 padding__LeftRight">
+                      All Topics
+                    </a>
+                  </div>
+                  <div class="navbar-item has-dropdown is-hoverable font_footer_prompt_15">
+                    <a class="navbar-link set-blue blue colorWhite padding__LeftRight" href="#!">
+                      OHO Topic
+                    </a>
+                  </div>
+                  <div class="navbar-item has-dropdown is-hoverable font_footer_prompt_15">
+                    <a class="navbar-link white colorWhite padding__LeftRight" href="#!">
+                      เกี่ยวกับเรา
+                    </a>
+                  </div>
+                  <div class="navbar-item has-dropdown is-hoverable font_footer_prompt_15">
+                    <a class="navbar-link white colorWhite padding__LeftRight" href="#!">
+                      ติดต่อโฆษณา
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="column is-3">
+              <div class="social">
+                <a class="is-inverted">
+                  <img src="../../images/icon/if_twitter_252077.svg" class="social-twitter-icon">
+                </a>
+                <a class="is-inverted">
+                  <img src="../../images/icon/if_facebook_986944.svg" class="social-facebook-icon">
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  `
+}
+
 
