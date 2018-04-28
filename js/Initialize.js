14982716e63f4fbd658885d2997703641edaff00
sittisuk <beerxp1@gmail.com>
2018-04-28 13:04:20 +0700

    profile can click to topic sure

--- js/Initialize.js
+++ js/Initialize.js
@@ -1,10 +1,26 @@
+const config = {};
+config.connectServer = {
+    linkfreeServ: 'https://oohoserver.000webhostapp.com/fetch.php',
+    linkServ: 'https://81021100-52-20180323161333.webstarterz.com/fetchTopic.php'
+}
+
+
+
+
+
+
+
+
+
 // Initialize Firebase
-var config = {
-  apiKey: "AIzaSyCQCcB3ehfHl95rgFaDiKDKLW-w8v683Mc",
-  authDomain: "ooho-001.firebaseapp.com",
-  databaseURL: "https://ooho-001.firebaseio.com",
-  projectId: "ooho-001",
-  storageBucket: "ooho-001.appspot.com",
-  messagingSenderId: "499976697391"
-};
-firebase.initializeApp(config);
+// var config = {
+//   apiKey: "AIzaSyCQCcB3ehfHl95rgFaDiKDKLW-w8v683Mc",
+//   authDomain: "ooho-001.firebaseapp.com",
+//   databaseURL: "https://ooho-001.firebaseio.com",
+//   projectId: "ooho-001",
+//   storageBucket: "ooho-001.appspot.com",
+//   messagingSenderId: "499976697391"
+// };
+// firebase.initializeApp(config);
+
+
